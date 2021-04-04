a940b231fa898538998f0e944dc2b8d08be75daf
mihomatsui <mihomatsui.24@gmail.com>
2021-04-04 16:39:18 +0900

    activerecord-importを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
   CSV_COLUMNS = %w[name age height].freeze
 
   def self.import_csv(file:)
-    # トランザクション(CSVインポートの途中でエラーが出た場合はロールバック)
-    User.transaction do
-      CSV.foreach(file.path, headers: true) do |row|
-        # インポートするカラムを制限
-        create!(row.to_h.slice(*CSV_COLUMNS))
-      end
+    list = []
+    CSV.foreach(file.path, headers: true) do |row|
+      list << row.to_h.slice(*CSV_COLUMNS)
     end
+    User.import!(list)
   end
 end
